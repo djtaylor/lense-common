3bb3349b548f2e925458ca0a31f5a364ed34e67d
David Taylor <djtaylor13@gmail.com>
2015-10-15 16:57:19 -0400

    Added new usr/share/docs/lense content

---
+++ usr/share/doc/lense/README.md
@@ -0,0 +1,6 @@
+### Lense Documentation
+
+# Bootstrap Answers
+
+Please see the file 'answers.js' for an example on how to construct and answers file
+for the bootstrap process.
---
+++ usr/share/doc/lense/answers.js
@@ -0,0 +1,46 @@
+{
+	"engine": {
+		"db_host": "localhost",
+	    "db_port": "3306",
+	    "db_name": "lense",
+	    "db_user": "lense",
+	    "db_user_password": "secret",
+	    "db_root_password": "secret",
+	    "api_admin_password": "secret",
+	    "api_admin_email": "user@email.com",
+	    "api_host": "localhost",
+	    "api_port": "10550",
+	    "portal_host": "localhost",
+	    "portal_port": "80",
+	    "socket_host": "localhost",
+	    "socket_port": "10551"
+	},
+	"portal": {
+		"db_host": "localhost",
+	    "db_port": "3306",
+	    "db_name": "lense",
+	    "db_user": "lense",
+	    "db_user_password": "secret",
+	    "engine_host": "localhost",
+	    "engine_port": "10550",
+	    "portal_host": "localhost",
+	    "portal_port": "80",
+	    "socket_host": "localhost",
+	    "socket_port": "10551",
+	    "admin_user": "lense",
+	    "admin_group": "00000000-0000-0000-0000-000000000000"
+	},
+	"client": {
+		"admin_user": "lense",
+	    "admin_group": "00000000-0000-0000-0000-000000000000"
+	},
+	"socket": {
+		"admin_user": "lense",
+	    "admin_group": "00000000-0000-0000-0000-000000000000",
+	    "engine_host": "localhost",
+	    "engine_port": "10550",
+	    "socket_host": "localhost",
+	    "socket_port": "10551",
+	    "socket_bind_ipaddr": "127.0.0.1"
+	}
+}
