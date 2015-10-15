d46bac918ded1f19b5e6c6d7959d41addc239343
David Taylor <djtaylor13@gmail.com>
2015-10-15 11:20:06 -0400

    Fixed typo in lense engine bootstrap complete summary

--- usr/lib/python2.7/dist-packages/lense/common/bootstrap/manager.py
+++ usr/lib/python2.7/dist-packages/lense/common/bootstrap/manager.py
@@ -600,9 +600,9 @@ class _BootstrapEngine(_BootstrapCommon):
             'You should restart your Apache service to load the new virtual host',
             'configuration. In order to use the Lense CLI client you should add the',
             'following environment variables to your ~/.bashrc file:\n',
-            'export LENSE_API_USER = "{0}"'.format(self.params.user['username']),
-            'export LENSE_API_KEY = "{0}"'.format(self.params.user['group']),
-            'export LENSE_API_GROUP = "{0}"'.format(self.params.user['key'])  
+            'export LENSE_API_USER="{0}"'.format(self.params.user['username']),
+            'export LENSE_API_KEY="{0}"'.format(self.params.user['group']),
+            'export LENSE_API_GROUP="{0}"'.format(self.params.user['key'])  
         ], 'COMPLETE')
         
 class _BootstrapPortal(_BootstrapCommon):
