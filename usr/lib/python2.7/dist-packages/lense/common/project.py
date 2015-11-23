77d15714cb186da75455d0d3d83390546822880b
David Taylor <djtaylor13@gmail.com>
2015-11-23 10:29:20 -0500

    Moved projects to project

---
+++ usr/lib/python2.7/dist-packages/lense/common/project.py
@@ -0,0 +1,45 @@
+from lense.common.vars import PROJECTS, TEMPLATES
+from lense.common.exceptions import InvalidProjectID
+
+class _LenseProjectLog(object):
+    """
+    Private class for mapping project log attributes.
+    """
+    def __init__(self, project):
+        """
+        @param project: The target project
+        @type  project: str
+        """
+        self.name = 'lense.{0}'.format(project)
+        self.file = getattr(getattr(PROJECTS, project), 'LOG', None)
+
+class LenseProject(object):
+    """
+    Class for storing project attributes.
+    """
+    def __init__(self, project):
+        """
+        @param project: The target project
+        @type  project: str
+        """
+        if not hasattr(PROJECTS, project):
+            raise InvalidProjectID(project)
+        
+        # Project name
+        self.name        = project
+        
+        # Get the project attributes
+        self._attrs      = getattr(PROJECTS, project, None)
+        
+        # Configuration and log files
+        self.log         = _LenseProjectLog(project)
+        self.conf        = getattr(self._attrs, 'CONF', None)
+        
+        # Project templates
+        self.TEMPLATES   = getattr(TEMPLATES, project, None)
+        
+        # Use a Django request object or not
+        self.use_request = getattr(self._attrs, 'REQUEST', False)
+        
+        # Use the objects manager or not
+        self.use_objects = getattr(self._attrs, 'OBJECTS', False)
