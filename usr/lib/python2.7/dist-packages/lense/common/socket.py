fdf8307d1f738f3279976b75d4538f29aef72c42
David Taylor <djtaylor13@gmail.com>
2015-11-29 21:21:12 -0500

    Common socket module

---
+++ usr/lib/python2.7/dist-packages/lense/common/socket.py
@@ -0,0 +1,54 @@
+from socketIO_client import SocketIO
+
+class LenseSocketIO(object):
+    """
+    Class objects for handling SocketIO interactions.
+    """
+    def __init__(self):
+        self.io     = None
+        self.params = None
+        
+        # Try to open the connection
+        try:
+            if LENSE.CONF.socket.enable:
+                self.io = SocketIO(LENSE.CONF.socket.host, int(LENSE.CONF.socket.port))
+                
+                # Socket connection opened sucessfully
+                LENSE.LOG.info('Initialized SocketIO proxy connection -> {0}:{1}'.format(LENSE.CONF.socket.host, LENSE.CONF.socket.port))
+            else:
+                LENSE.LOG.info('SocketIO proxy server disabled - skipping...')
+            
+        # Critical error when opening connection
+        except Exception as e:
+            LENSE.LOG.info('Failed to initialize SocketIO proxy connection: {}'.format(str(e)))
+        
+    def set(self, params):
+        """
+        Set the web socket client attributes.
+        """
+        self.params = params
+        return params
+        
+    def disconnect(self):
+        """
+        Disconnect the Socket.IO client.
+        """
+        try:
+            self.io.disconnect()
+            LENSE.LOG.info('Closing SocketIO proxy connection')
+        except:
+            pass
+        
+    def broadcast(self, t, d={}):
+        """
+        Broadcast data to all web socket clients.
+        """
+        if self.io and LENSE.CONF.socket.enable:
+            self.io.emit('update', {'type': t, 'content': d})
+        
+    def loading(self, m=None):
+        """
+        Send a loading messages to a web socket client.
+        """
+        if self.params and self.io and LENSE.CONF.socket.enable:
+            self.io.emit('update', { 'room': self.params['room'], 'type': 'loading', 'content': m})
