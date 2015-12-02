eef776effce7456acf401076fc2adb9f51b44193
David Taylor <djtaylor13@gmail.com>
2015-12-01 21:18:08 -0500

    Testing bare bones commons settings file

---
+++ usr/lib/python2.7/dist-packages/lense/bootstrap/settings.py
@@ -0,0 +1,26 @@
+from lense.common.utils import rstring
+
+# Debug mode
+DEBUG            = True
+
+# Secret key
+SECRET_KEY       = rstring(64)
+
+# Internationalization settings
+LANGUAGE_CODE    = 'en-us'
+TIME_ZONE        = 'UTC'
+USE_I18N         = True
+USE_L10N         = True
+USE_TZ           = True
+
+# URL processor
+ROOT_URLCONF     = __name__
+
+# Django middleware classes
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
