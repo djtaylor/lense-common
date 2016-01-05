1d9e38fc5ca658148e3560102062cedb1071102c
David Taylor <djtaylor13@gmail.com>
2016-01-05 13:52:59 -0500

    Removing legacy return methods in request handlers

--- usr/lib/python2.7/dist-packages/lense/common/http.py
+++ usr/lib/python2.7/dist-packages/lense/common/http.py
@@ -4,6 +4,7 @@ import json
 import traceback
 
 # Django Libraries
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 
 # Lense Libraries
@@ -71,79 +72,6 @@ MIME_TYPE = Collection({
     }
 }).get()
 
-def error_response(message, response=None, cli=False):
-    """
-    Helper method used to handle a returned error response.
-    
-    :param message:  The error message to display
-    :type message:   str
-    :param response: A Python response object
-    :type response:  obj
-    :param cli:      Boolean indicating if being called from a command line client
-    :type cli:       bool
-    """
-    
-    # If being run from the command line
-    if cli:
-        
-        # Show the error message
-        print('ERROR: {0}'.format(message))
-        
-        # Show any problems with token retrieval
-        if response:
-            rsp_msg = response['body'].get('message', 'Failed to process the request')
-            rsp_err = response['body'].get('error', 'An unknown error has occurred')
-            
-            # Print the response
-            print('\n---RESPONSE---')
-            print('HTTP {0}: {1}\n'.format(response['code'], '{0} - {1}'.format(rsp_msg, rsp_err)))
-            
-            # If any debug information is present
-            if 'debug' in response['body']:
-                print('---DEBUG---')
-                print('Traceback (most recent call last):')
-                for l in response['body']['debug']['traceback']:
-                    print('  File "{0}", line {1}, in {2}'.format(l[0], l[1], l[2]))
-                    print('    {0}'.format(l[3]))
-                print('Exception: {0}\n'.format(response['body']['debug']['exception']))
-        
-        # Exit the client
-        sys.exit(1)
-        
-    # Library is being loaded from another module
-    else:
-        return response
-
-def parse_response(obj):
-    """
-    Helper method used to parse a Python requests object and return a formatted dictionary.
-    Used to help alleviate differences between versions of the requests module on different
-    systems.
-    
-    :param obj: The response object to parse
-    :type obj: object
-    :rtype: dict
-    """
-    return_obj = {}
-    
-    # Look for the status code
-    if hasattr(obj, 'status_code'):
-        return_obj['code'] = obj.status_code
-    if hasattr(obj, 'code'):
-        return_obj['code'] = obj.code
-    
-    # Look for the return body
-    if hasattr(obj, 'content'):
-        return_obj['body'] = obj.content
-    if hasattr(obj, 'text'):
-        if callable(getattr(obj, 'text')):
-            return_obj['body'] = obj.text()
-        else:
-            return_obj['body'] = obj.text
-    
-    # Return the formatted response
-    return return_obj
-
 class JSONSuccess(object):
     """
     Base class for successfull request responses.
@@ -152,11 +80,14 @@ class JSONSuccess(object):
         self.msg  = msg
         self.data = data
 
+        # Response body
+        self.body = json.dumps({'message': self.msg, 'data': self.data}, cls=DjangoJSONEncoder)
+
     def response(self):
         """
         Construct and return the HTTP 200 response.
         """
-        return HttpResponse(json.dumps({'message': self.msg, 'data': self.data}), content_type=MIME_TYPE.APPLICATION.JSON, status=200)
+        return HttpResponse(self.body, content_type=MIME_TYPE.APPLICATION.JSON, status=200)
 
 class JSONErrorBase(object):
     """
@@ -170,7 +101,6 @@ class JSONErrorBase(object):
         # Construct the JSON error object
         self.error_object = {
             'message': ERR_MESSAGE.get(self.status, 'An unknown error has occurred, please contact your administrator'),
-            'code':    self.status,
             'error':   error if not isinstance(error, (list, dict)) else json.dumps(error)
         }
         
